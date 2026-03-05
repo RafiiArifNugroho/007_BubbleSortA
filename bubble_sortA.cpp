005a878e28792ccbbd274b65872a139c848259bd
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-03-05 15:49:38 +0700

    Menambahkan input elemen array

--- bubble_sortA.cpp
+++ bubble_sortA.cpp
@@ -38,3 +38,13 @@ void input()
             cout << "Maksimal banyaknya elemen adalah 20";
         }
     }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukan elemen array" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
