4808010f724928e9e2d60eb56fce31f8d77bfbac
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-03-05 15:58:43 +0700

    Menambahkan algoritma Bubble Sort pada array

--- bubble_sortA.cpp
+++ bubble_sortA.cpp
@@ -2,49 +2,92 @@
 
 #include <iostream>
 #include <string>
-using namespace std;
+using namespace std; // Library
 
-int arr[20]; // Deklarasi Variable Global array a dengan ukuran 20
-int n;       // Deklarasi variable global n untuk menyimpan banyaknya
+int arr[20]; // Deklarasi Variabel global
+int n;
 
 void input()
 {
-    // procedur untuk input
-    int d;
+    int d; // Procedure input
     while (true)
     {
-        cout << "Masukkan banyaknya elemen pada array : ";
+        cout << "Enter the number of elements in the array: ";
         cin >> n;
         if (n <= 20)
             break;
         else
         {
-            cout << "n\Array dapat mempunyai 20 elemen.\n";
+            cout << "Error! number should be in range of 1-20." << endl;
         }
     }
-}
 
-void input()
-{
-    int d;
-    while (true)
-    {
-        cout << "Masukan banyaknya elemnt (max 20): ";
-        cin >> n;
-        if (n <= 20)
-            break;
-        else
-        {
-            cout << "Maksimal banyaknya elemen adalah 20";
-        }
-    }
     cout << endl;
-    cout << "====================" << endl;
-    cout << "Masukan elemen array" << endl;
+    cout << "-------------------" << endl;
+    cout << "Enter Array Element" << endl;
+    cout << "-------------------" << endl;
 
     for (int i = 0; i < n; i++)
     {
-        cout << "Data ke-" << (i + 1) << ": ";
+        cout << "Enter element " << (i + 1) << ": ";
         cin >> arr[i];
     }
+}
+
+void bubbleSortArray()
+{
+    int pass = 1;
+
+    do
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        pass += 1;
+
+        cout << "\nPass " << pass - 1 << ": ";
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+
+    } while (pass <= n - 1);
+}
+
+void display()
+{
+    cout << endl;
+    cout << "-------------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "-------------------" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j];
+        if (j < n - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "amount of passes: " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    bubbleSortArray();
+    display();
+    system("pause");
+    return 0;
 }
